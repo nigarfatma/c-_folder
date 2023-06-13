0819c12237c4569be14667e16b2e963e5f453f0a
Nigar Fatma <nigarfatma180@gmail.com>
2023-06-14 01:53:56 +0530

    initial commit

---
+++ Hello.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <limits.h>
+
+// using namespace std;
+// int main()
+// {
+// 1.
+// cout << "Hello \n";
+// cout << "World";
+// return 0;
+
+// 2.
+// cout << "Hello"
+//      << "World";
+// return 0;
+
+// 3.
+// cout << "Hello" << endl;
+// cout << "World";
+// return 0;
+// char a = -500;
+// cout << a;
+//     cout << CHAR_MIN << " " << CHAR_MAX;
+//     return 0;
+// }
+
+// datatypes
+
+// Variables
---
+++ Variable.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <limits.h>
+#include <string>
+using namespace std;
+int main()
+{
+    // int a = 5;
+    // float b = 6.7;
+    // double c = 4.21;
+    // bool d = true;
+    // bool d=True;  it give error
+    // char e = 'c';
+    //   char e="ab"  //it give errro
+    // string s = "abcd";
+    //   const int a=5;
+    //     a=7;
+    //     return 0;
+    // int a = 5;
+    // float b = a;
+    // double b = a;
+    // long b = a;
+    // int a = 65;
+    // char b = (char)a;
+    // cout << a << " " << b << endl;
+    // return 0;
+    // char a = '5';
+    // char a = '0';
+
+    // char a = 0;
+    // bool b = (bool)a;
+    // cout << a << " " << b;
+    // cout << "\\n";
+    // int a = 56;
+    // string b = to_string(a);
+    // cout << a << " " << b << endl;
+
+    // string to boolean
+    // string a = "hello";
+    string a = "121";
+    bool b = (bool)stoi(a);
+    cout << b;
+    // cout << a << " " << b;
+    // input by user
+    // int a;
+    // cin >> a;
+    // cout << "value of a is" << a;
+
+    return 0;
+}
